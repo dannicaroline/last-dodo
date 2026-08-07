512dd43b847913afe8ed4217103a3e2dc750d114
dannicaroline <124710226+dannicaroline@users.noreply.github.com>
2026-08-06 21:29:07 -0300

    Update dados.s

--- dados.s
+++ dados.s
@@ -7,7 +7,7 @@ fase_atual:    .word 1
 frame_atual:   .word 0      
 direcao_dodo:  .word 100    
 
-player_hp:     .word 3      # <-- Definido apenas UMA VEZ aqui!
+player_hp:     .word 3      
 
 player_x:      .word 32       
 player_y:      .word 32
@@ -15,19 +15,19 @@ old_x:         .word 32
 old_y:         .word 32
 
 # =========================================================
-# POSIÇÃO E CONTROLE DOS INIMIGOS (Mapas 1 e 2)
+# POSIÃ‡ÃƒO E CONTROLE DOS INIMIGOS (Mapas 1 e 2)
 # =========================================================
 rato_x:             .word 128    
 rato_y:             .word 112
 rato_dir:           .word 16     
-rato_olhando_dir:   .word 1      # 1 = Direita, 0 = Esquerda
+rato_olhando_dir:   .word 1     
 
 cachorro_x:         .word 265  
 cachorro_y:         .word 64
 cachorro_olhando_dir: .word 1    
 
 # =========================================================
-# EXPORTAÇÃO DE SÍMBOLOS GLOBAIS
+# EXPORTAÃ‡ÃƒO DE SÃMBOLOS GLOBAIS
 # =========================================================
 .globl player_x, player_y, old_x, old_y, fase_atual, frame_atual, direcao_dodo
 .globl player_hp, rato_x, rato_y, rato_dir, rato_olhando_dir
@@ -48,7 +48,7 @@ mapa2:
     .include "mapa2.data"  
 
 # =========================================================
-# INCLUSÃO DE SPRITES DO JOGADOR E CENÁRIO
+# INCLUSÃƒO DE SPRITES DO JOGADOR E CENÃRIO
 # =========================================================
 .include "dodo.data"
 .include "madeira_escura.data"    
@@ -66,7 +66,7 @@ sprite_madeira:
     .include "madeira.data"
 
 # =========================================================
-# INCLUSÃO DE SPRITES DOS INIMIGOS
+# INCLUSÃƒO DE SPRITES DOS INIMIGOS
 # =========================================================
 .align 2
 sprite_rato:
